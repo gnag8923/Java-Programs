e8bb3b42f24ac7a4cd01a3105ee5630e811861c3
gnag8923 <109598927+gnag8923@users.noreply.github.com>
2026-08-14 19:28:44 +0530

    Add alternative array for testing non-decreasing order
    Added a commented-out alternative array for testing.

--- NonDecreasingOrder.java
+++ NonDecreasingOrder.java
@@ -1,6 +1,7 @@
 public class NonDecreasingOrder {
 	public static void main(String []args) {
 		int [] arr = {1, 2,3, 4, 5, 6, 7, 8};
+		//int [] arr = {1, 42,33, 34, 23, 12, 55, 56, 38,77};
 		
 		for(int i=0;i<arr.length-1;i++) {
 			if(arr[i]>arr[i+1]) {
